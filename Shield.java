181a23dffe6b7fea117adb22ed73c2cc54040e67
elisego <elise.godard@gmail.com>
2020-06-08 22:49:42 +0200

    strem 01 filter

--- Shield.java
+++ Shield.java
@@ -4,7 +4,7 @@ import java.util.stream.Collectors;
 
 public class Shield {
 
-    public static void main(String[] args) {
+    public static <list> void main(String[] args) {
 
         List<Hero> heroes = new ArrayList<>();
         heroes.add(new Hero("Black Widow", 34, false));
@@ -17,16 +17,21 @@ public class Shield {
         heroes.add(new Hero("Hulk", 49, true));
         heroes.add(new Hero("Doctor Strange", 42, false));
 
-        List<Hero> elders;
         // TODO 1 : filter heroes in order to found heroes older than 59
+        List<Hero> elders = heroes.stream()
+                .filter (item -> item.getAge() > 59)
+                .collect(Collectors.toList());
 
         System.out.println("\nElders:");
         for (Hero elder : elders) {
             System.out.println(elder.getName());
         }
 
-        List<Hero> intolerants;
         // TODO 2 : filter heroes in order to found heroes that are gluten intolerants
+        List<Hero> intolerants = heroes.stream()
+            .filter(item -> item.isGlutenIntolerant())
+                .collect(Collectors.toList());
+
 
         System.out.println("\nGluten intolerants:");
         for (Hero intolerant : intolerants) {
